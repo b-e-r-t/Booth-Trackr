7585d6686d440df6992da6931311b5e32c692315
b-e-r-t <bertozarate95@gmail.com>
2024-06-10 18:26:02 -0700

    MONGO DB
    Mongo DB CONNECTION SUCCESFUL

--- booth-trackr/src/app/page.tsx
+++ booth-trackr/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  run().catch(console.dir);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -111,3 +112,27 @@ export default function Home() {
     </main>
   );
 }
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://bertozarate95:boothtrackr.2.@gcloud0.w6ua4zp.mongodb.net/?retryWrites=true&w=majority&appName=gcloud0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+//run().catch(console.dir);
