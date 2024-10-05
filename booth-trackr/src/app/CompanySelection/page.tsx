91c8d79c439a6bd1e156c6e28d975813f9aba115
yichiii <55825777+yichii@users.noreply.github.com>
2024-10-04 20:14:07 -0700

    Company Selection Almost Complete
    Need to add functionality for companySelection page

--- booth-trackr/src/app/CompanySelection/page.tsx
+++ booth-trackr/src/app/CompanySelection/page.tsx
@@ -1,12 +1,24 @@
-import Image from 'next/image'
-import { BsChevronRight } from "react-icons/bs";
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
+import Select from "react-select";
+
+const Hotels = [
+  { value: 1, label: "Apple" },
+  { value: 2, label: "Bank of America" },
+  { value: 3, label: "Cisco" },
+];
 
 export default function EventSelection() {
-    return (
-        <div className="hero bg-content-400 min-h-screen">
-            <h1>This is the company selection screen</h1>
+  return (
+    <div className="hero bg-content-400 min-h-screen">
+      <div className="flex justify-center items-center flex-col">
+        <div className="">
+          <h1>Select Companies to Track</h1>
         </div>
-    );
+        <div className="px-2">
+          <Select name="CompanySelection" isMulti options={Hotels} />
+        </div>
+      </div>
+    </div>
+  );
 }
-
