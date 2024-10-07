e36a3d54d054a778557ab0031f42b8160e468e29
b-e-r-t <73658605+b-e-r-t@users.noreply.github.com>
2024-10-06 19:02:35 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,16 @@ export default function Home (){
             </div>
         </main>
         */
-        <div className="p-7 max-w-sm mx-auto bg-white rounded-xl shadow-md items-center space-x-4">
-        <div className='flex-shrink-9'>
-          <img className="h-13 w-12" src="/boothlogo.png" alt="ChitChat Logo"/>
-        </div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
+       <main className = " py-96" >
+        <div className=" flow-root p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md items-center space-x-4">
+          <div className='py-px'>
+            <img className="my-10 h-38 w-36 " src="/boothlogo.png" alt="ChitChat Logo"/>
+          </div>
+          <div className=" text-xl font-medium text-black">ChitChat</div>
           <p className="text-gray-500">You have a new message!</p>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
         </div>
-      </div>
+      </main> 
         
     )
 
