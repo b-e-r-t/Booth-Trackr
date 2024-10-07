69df920522f3a5aa7efd2308973c9ea6c16ba904
b-e-r-t <73658605+b-e-r-t@users.noreply.github.com>
2024-10-06 17:02:06 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export default function Home (){
     return (
+        /*
         <main className= 'container mx-auto p-4'>
             <h1 className="text-balance text-4xl mx-auto fixed h-40 flex font-bold tracking-tight text-gray-100 sm:text-6xl ">
               BOOTHTRACKR
@@ -16,6 +17,17 @@ export default function Home (){
             /> 
             </div>
         </main>
+        */
+        <div className="p-7 max-w-sm mx-auto bg-white rounded-xl shadow-md items-center space-x-4">
+        <div className='flex-shrink-9'>
+          <img className="h-13 w-12" src="/boothlogo.png" alt="ChitChat Logo"/>
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+        
     )
 
 }
