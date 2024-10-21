a45b46ab456b599f36dd30e7f039a7ecf8993818
Naz Diam <55825777+yichii@users.noreply.github.com>
2024-10-20 23:26:08 -0700

    Almost done with passing companylist to home page
    Need to do:
    - in companySelection page, modify the onChange prop for multiselect component
    
    Some threads that could help:
    https://stackoverflow.com/questions/71094599/how-to-get-value-from-react-select-form
    https://medium.com/@bseleng/react-typescript-ts2345-argument-of-type-null-is-not-assignable-to-parameter-of-type-80a445d98dd1

--- src/app/CompanySelection/page.tsx
+++ src/app/CompanySelection/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
-import Select, { components, MultiValueGenericProps } from "react-select";
+import Select, { components, MultiValue, MultiValueGenericProps, OnChangeValue } from "react-select";
+import { useSearchParams } from "next/navigation";
+import React, { useState } from "react";
 
 const companies = [
   { value: 1, label: "Apple" },
@@ -14,7 +16,18 @@ const MultiValueLabel = (props: MultiValueGenericProps) => {
   return <components.MultiValueLabel {...props} />;
 };
 
+const AnotherPage = ({ searchParams }: { searchParams: string }) => {
+  console.log(searchParams.search); // Logs "search"
+};
+
 export default function EventSelection() {
+  const [companyList, setCompanyList] = useState<MultiValue<unknown> | null>(null)
+  // const searchParams = useSearchParams();
+  // console.log(searchParams.get("search"));
+
+  // const onChange = (selectedOptions: OnChangeValue<companies, true>) =>
+  //   setSelected(selectedOptions);
+
   return (
     <div className="hero bg-content-400 min-h-screen">
       <div className="flex justify-center items-center flex-col">
@@ -32,10 +45,20 @@ export default function EventSelection() {
             }}
             isMulti
             options={companies}
+            onChange={(choice) => setCompanyList(choice)}
           />
         </div>
         <button className="btn btn-primary bg-lime-600 text-white border-none hover:text-lime-600 hover:bg-white">
-          <Link href="/DetailSelection">Add Companies</Link>
+          <Link
+            href={{
+              pathname: "/DetailSelection",
+              query: {
+                search: companyList,
+              },
+            }}
+          >
+            Add Companies
+          </Link>
         </button>
       </div>
     </div>
--- src/app/DetailSelection/page.tsx
+++ src/app/DetailSelection/page.tsx
@@ -1,12 +1,20 @@
 "use client";
-
+import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 import { BsChevronRight } from "react-icons/bs";
 import Link from "next/link";
 import React, { useState } from "react";
 import ButtonToggle from "../UI/ButtonToggle";
 
-export default function DetailSelection({ }) {
+const AnotherPage = ({ searchParams }: { searchParams: string }) => {
+  console.log(searchParams.search); // Logs "search"
+};
+
+export default function DetailSelection({}) {
+  const searchParams = useSearchParams();
+  console.log(searchParams.get('search')); // Logs "search"
+  const AAAA = searchParams.get('search');
+  
   const [active, setActive] = useState(false);
   const handleClick = () => {
     setActive(!active);
@@ -17,30 +25,41 @@ export default function DetailSelection({ }) {
       <div className="flex justify-center items-center flex-col gap-4">
         <h1>What would you like to track?</h1>
         <div className="flex justify-center items-center flex-row gap-4">
-          <ButtonToggle name={"Term Hiring"}/>
+          <ButtonToggle name={"Term Hiring"} />
           <ButtonToggle name={"Recruiter Name"}></ButtonToggle>
         </div>
         <div className="flex justify-center items-center flex-row gap-4">
-          <ButtonToggle name={"Currently Hiring"}/>
+          <ButtonToggle name={"Currently Hiring"} />
           <ButtonToggle name={"Booth Number"}></ButtonToggle>
         </div>
         <div className="flex justify-center items-center flex-row gap-4">
-          <ButtonToggle name={"Website URL"}/>
+          <ButtonToggle name={"Website URL"} />
           <ButtonToggle name={"Recruiter Contact Info"}></ButtonToggle>
           <ButtonToggle name={"Date Visited"}></ButtonToggle>
         </div>
         <div className="flex justify-center items-center flex-row gap-4">
-          <ButtonToggle name={"Application Deadline"}/>
+          <ButtonToggle name={"Application Deadline"} />
           <ButtonToggle name={"Employment Location"}></ButtonToggle>
         </div>
         <div className="flex justify-center items-center flex-row gap-4">
-          <ButtonToggle name={"Application Status"}/>
+          <ButtonToggle name={"Application Status"} />
           <ButtonToggle name={"Custom Field"}></ButtonToggle>
           <ButtonToggle name={"Role"}></ButtonToggle>
         </div>
         <ButtonToggle name={"Select All"}></ButtonToggle>
-        
-        <button className="btn btn-primary">Add Fields</button>
+        {/* Doesn't work, please implement! */}
+        <button className="btn btn-primary bg-lime-600 text-white border-none hover:text-lime-600 hover:bg-white">
+          <Link
+            href={{
+              pathname: "/Home",
+              query: {
+                search: AAAA,
+              }
+            }}
+          >
+            Add Fields
+          </Link>
+        </button>
       </div>
     </div>
   );
