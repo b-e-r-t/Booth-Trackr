8f3962220b7dc3a0282403884b261cdb0471f901
yichiii <55825777+yichii@users.noreply.github.com>
2024-10-27 14:31:15 -0700

    fk this man
    still not done with passing companyList to home page

--- src/app/CompanySelection/page.tsx
+++ src/app/CompanySelection/page.tsx
@@ -4,29 +4,32 @@ import Select, { components, MultiValue, MultiValueGenericProps, OnChangeValue }
 import { useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
-const companies = [
-  { value: 1, label: "Apple" },
-  { value: 2, label: "Bank of America" },
-  { value: 3, label: "Cisco" },
-  { value: 4, label: "Disney" },
-  { value: 5, label: "Enterprise" },
-];
-
-const MultiValueLabel = (props: MultiValueGenericProps) => {
-  return <components.MultiValueLabel {...props} />;
-};
+// const MultiValueLabel = (props: MultiValueGenericProps) => {
+//   return <components.MultiValueLabel {...props} />;
+// };
 
 const AnotherPage = ({ searchParams }: { searchParams: string }) => {
   console.log(searchParams.search); // Logs "search"
 };
 
 export default function EventSelection() {
-  const [companyList, setCompanyList] = useState<MultiValue<unknown> | null>(null)
-  // const searchParams = useSearchParams();
-  // console.log(searchParams.get("search"));
+  const companies = [
+    { value: 1, label: "Apple" },
+    { value: 2, label: "Bank of America" },
+    { value: 3, label: "Cisco" },
+    { value: 4, label: "Disney" },
+    { value: 5, label: "Enterprise" },
+  ];
+  const [companyList, setCompanyList] = useState([]);
+  const handleChange = (selectedOption) => { 
+    console.log(selectedOption);
+    setCompanyList(selectedOption);
+  };
 
-  // const onChange = (selectedOptions: OnChangeValue<companies, true>) =>
-  //   setSelected(selectedOptions);
+  const submit = () => {
+    console.log("Company listfdasfsdafsdafas");
+    console.log("Company list: " + companyList);
+  }
 
   return (
     <div className="hero bg-content-400 min-h-screen">
@@ -37,18 +40,19 @@ export default function EventSelection() {
         <div className="px-2">
           <Select
             closeMenuOnSelect={false}
-            components={{ MultiValueLabel }}
-            styles={{
-              multiValue: (base) => ({
-                ...base,
-              }),
-            }}
-            isMulti
             options={companies}
-            onChange={(choice) => setCompanyList(choice)}
+            value={companyList}
+            onChange={handleChange}
+            isMulti={true}
+            // components={{ MultiValueLabel }}
+            // styles={{
+            //   multiValue: (base) => ({
+            //     ...base,
+            //   }),
+            // }}
           />
         </div>
-        <button className="btn btn-primary bg-lime-600 text-white border-none hover:text-lime-600 hover:bg-white">
+        <button className="btn btn-primary bg-lime-600 text-white border-none hover:text-lime-600 hover:bg-white" onSubmit={submit}>
           <Link
             href={{
               pathname: "/DetailSelection",
