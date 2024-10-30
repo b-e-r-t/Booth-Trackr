5014aba8284a1a43dd5c7361a1be89d2bef99e1b
b-e-r-t <73658605+b-e-r-t@users.noreply.github.com>
2024-10-29 17:50:43 -0700

    Update page.tsx

--- src/app/DetailSelection/page.tsx
+++ src/app/DetailSelection/page.tsx
@@ -20,9 +20,9 @@ export default function DetailSelection({}) {
   function handleClick () {
     setChildValue(!childValue);
   };
-  const handleChange = useCallback(() => {
-    setChildValue(!childValue);
-  }, [childValue]);
+  // const handleChange = useCallback(() => {
+  //   setChildValue(!childValue);
+  // }, [childValue]);
  
  
   return (
